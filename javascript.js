e899403523faacb8b8dca60edca20e66f71dfac7
nareshix <nareshix66@gmail.com>
2024-08-30 11:02:19 +0800

    changed var name for creating div

--- javascript.js
+++ javascript.js
@@ -1,9 +1,12 @@
 const container = document.querySelector('#container');
 
 for (let i = 0; i < (16*16); ++i){
-    const div = document.createElement('div');
-    div.textContent = '1';
-    div.style.aspectRatio = '1/1';
-    div.style.width = '50%';
-    container.append(div);
+    const newDiv = document.createElement('div');
+    newDiv.textContent = '1';
+    newDiv.classList = 'box';
+    newDiv.style.aspectRatio = '1/1';
+    newDiv.style.width = '50%';
+    container.append(newDiv);
 }
+
+const box = document.querySelector('box');
