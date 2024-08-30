858b6c12bb310fad84369d24a8d2c20c190be0a4
nareshix <nareshix66@gmail.com>
2024-08-30 13:30:33 +0800

    removed rounding so that boxes exactly fits the border

--- javascript.js
+++ javascript.js
@@ -8,8 +8,8 @@ const createGrid = (sides=16) => {
     container.style.height = '960px';
     container.style.border = 'solid rebeccapurple';
 
-    container.style.gridTemplateColumns = `repeat(${sides}, ${Math.round(960/sides)}px)`;
-    container.style.gridTemplateRows = `repeat(${sides}, ${Math.round(960/sides)}px)`;
+    container.style.gridTemplateColumns = `repeat(${sides}, ${960/sides}px)`;
+    container.style.gridTemplateRows = `repeat(${sides}, ${960/sides}px)`;
 
     for (let i = 0; i < (sides*sides); ++i){
         const newDiv = document.createElement('div');
