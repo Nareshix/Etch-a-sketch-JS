fae8ad89b38e8d77cb52547465fdb526f3e22a17
nareshix <nareshix66@gmail.com>
2024-08-30 12:00:52 +0800

    removed line that is causing the box to not be hihglighted fully upon hover

--- javascript.js
+++ javascript.js
@@ -3,7 +3,6 @@ const container = document.querySelector('#container');
 for (let i = 0; i < (16*16); ++i){
     const newDiv = document.createElement('div');
     newDiv.style.aspectRatio = '1/1';
-    newDiv.style.width = '50%';
     newDiv.classList = 'box';
     container.append(newDiv);
 }
