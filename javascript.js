990c532bafa7b6d379f74daaa718ab91f25a305a
nareshix <nareshix66@gmail.com>
2024-08-30 10:58:37 +0800

    created 1 16x16 grid and create 16x16 divs

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 const container = document.querySelector('#container');
 
-for (let i = 0; i < 16; ++i){
+for (let i = 0; i < (16*16); ++i){
     const div = document.createElement('div');
-    div.textContent = 'square';
+    div.textContent = '1';
     div.style.aspectRatio = '1/1';
     div.style.width = '50%';
     container.append(div);
