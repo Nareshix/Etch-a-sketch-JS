7da975bc40820e1573c66f602b02026e6e27c4c6
nareshix <nareshix66@gmail.com>
2024-08-30 12:49:15 +0800

    grouped different features into funcitons so that i can easily reuse them

--- javascript.js
+++ javascript.js
@@ -1,17 +1,30 @@
 const container = document.querySelector('#container');
+const button = document.querySelector('button');
 
-for (let i = 0; i < (16*16); ++i){
-    const newDiv = document.createElement('div');
-    newDiv.style.aspectRatio = '1/1';
-    newDiv.classList = 'box';
-    newDiv.style.border = '1px solid black'
-    container.append(newDiv);
+const createGrid = (sides=16) => {
+    for (let i = 0; i < (sides*sides); ++i){
+        const newDiv = document.createElement('div');
+        newDiv.style.aspectRatio = '1/1';
+        newDiv.classList = 'box';
+        newDiv.style.border = '1px solid black'
+        container.append(newDiv);
+    }    
 }
 
-const boxes = document.querySelectorAll('.box');
+const enableColouring = () => {
+    let boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+        box.addEventListener('mouseover', (event) =>{
+            event.target.style.backgroundColor = 'blue'
+        })
+    });    
+}
 
-boxes.forEach(box => {
-    box.addEventListener('mouseover', (event) =>{
-        event.target.style.backgroundColor = 'blue'
-    })
-});
+button.addEventListener('click', () => {    // console.log(Number(window.prompt()));
+    sides = Number(window.prompt());
+    createGrid(sides);
+    enableColouring()
+})
+
+createGrid();
+enableColouring();
