ee62638af5905548e9a382515753fd0ee27b8780
nareshix <nareshix66@gmail.com>
2024-08-30 12:00:33 +0800

    added function where colour of box changes to blue upon hover

--- javascript.js
+++ javascript.js
@@ -2,11 +2,16 @@ const container = document.querySelector('#container');
 
 for (let i = 0; i < (16*16); ++i){
     const newDiv = document.createElement('div');
-    newDiv.textContent = '1';
-    newDiv.classList = 'box';
     newDiv.style.aspectRatio = '1/1';
     newDiv.style.width = '50%';
+    newDiv.classList = 'box';
     container.append(newDiv);
 }
 
-const box = document.querySelectorAll('box');
+const boxes = document.querySelectorAll('.box');
+
+boxes.forEach(box => {
+    box.addEventListener('mouseover', (event) =>{
+        event.target.style.backgroundColor = 'blue'
+    })
+});
